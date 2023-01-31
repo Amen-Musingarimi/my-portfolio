17d1b68e0e30ca1787b71e8b96e62f8d10ad1d72
Amen T. Musingarimi <takudzwamusinga@gmail.com>
2023-01-31 15:04:26 +0200

    Fix ES lint error

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
-"use strict";
+const btnOpenMenu = document.querySelector('.openModal');
+const btnCloseMenu = document.querySelector('.closeModal');
+const modalMenu = document.querySelector('.menuContainer');
+const navLinks = document.querySelectorAll('.nav-link');
+const body = document.querySelector('body');
 
-const btnOpenMenu = document.querySelector(".openModal");
-const btnCloseMenu = document.querySelector(".closeModal");
-const modalMenu = document.querySelector(".menuContainer");
-const navLinks = document.querySelectorAll(".nav-link");
-const body = document.querySelector("body");
+// Opening mobile menu
+const openMenu = function openModal() {
+  modalMenu.classList.remove('hidden');
+  modalMenu.classList.add('overlay');
+  btnCloseMenu.classList.remove('hidden');
+  btnOpenMenu.classList.add('hidden');
+  body.classList.add('blurry-text');
+};
 
-btnOpenMenu.addEventListener("click", function () {
-  modalMenu.classList.remove("hidden");
-  modalMenu.classList.add("overlay");
-  btnCloseMenu.classList.remove("hidden");
-  btnOpenMenu.classList.add("hidden");
-  body.classList.add("blurry-text");
-});
+btnOpenMenu.addEventListener('click', openMenu);
 
-btnCloseMenu.addEventListener("click", function () {
-  modalMenu.classList.add("hidden");
-  modalMenu.classList.remove("overlay");
-  btnCloseMenu.classList.add("hidden");
-  btnOpenMenu.classList.remove("hidden");
-  body.classList.remove("blurry-text");
-});
+// Close menu button
+const closeMenuBtn = function closeMenu() {
+  modalMenu.classList.add('hidden');
+  modalMenu.classList.remove('overlay');
+  btnCloseMenu.classList.add('hidden');
+  btnOpenMenu.classList.remove('hidden');
+  body.classList.remove('blurry-text');
+};
+btnCloseMenu.addEventListener('click', closeMenuBtn);
 
-for (let i = 0; i < navLinks.length; i++) {
-  navLinks[i].addEventListener("click", function () {
-    modalMenu.classList.add("hidden");
-    modalMenu.classList.remove("overlay");
-    btnCloseMenu.classList.add("hidden");
-    btnOpenMenu.classList.remove("hidden");
-    body.classList.remove("blurry-text");
-  });
+// Close menu after clicking menu option
+const closeOnClick = function close() {
+  modalMenu.classList.add('hidden');
+  modalMenu.classList.remove('overlay');
+  btnCloseMenu.classList.add('hidden');
+  btnOpenMenu.classList.remove('hidden');
+  body.classList.remove('blurry-text');
+};
+
+for (let i = 0; i < navLinks.length; i += 1) {
+  navLinks[i].addEventListener('click', closeOnClick);
 }
