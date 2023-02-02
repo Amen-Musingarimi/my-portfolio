9a2571a86b287a2e387cfd3e1c63c74f48a88946
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-02-02 16:39:44 +0200

    Fix eslint error

--- script.js
+++ script.js
@@ -39,8 +39,8 @@ for (let i = 0; i < navLinks.length; i += 1) {
 }
 
 // -----------------------------Popup window------------------------------------------------
-const descriptionText =
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent";
+const descriptionText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent";
+
 const portfolioArray = [
   {
     projectName: 'Tonic',
